f63eadb1b3b3573c921f87eb44f4756f97c6bec1
anidaameliaa <anidaamelia35@gmail.com>
2025-03-09 13:12:15 -0800

    update index.php

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
 </head>
 <body>
     <h1>Data Mahasiswa</h1>
-    <?php echo "ini data mahasiswa";?>
+    <br>
+    <table border="1" cellspacing="0" cellpadding"S">
+        <thread>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+    </thread>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>E020323085</td>
+            <td>Amel</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>E020323108</td>
+            <td>Wika</td>
+        </tr>
+        </tbody>
+    </table>
 </body>
 </html>
