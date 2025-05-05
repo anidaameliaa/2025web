cc84b6529b1c1366b61732ba805f7fff669be859
anidaameliaa <anidaamelia35@gmail.com>
2025-05-04 16:42:41 -0800

    menambah template

--- index.php
+++ index.php
@@ -8,52 +8,98 @@ include "koneksi.php";
 
 $query = "SELECT m.*, p.nama namaProdi FROM mahasiswa m JOIN prodi p ON m.id = p.id";
 $data = ambildata($query);
+
+include "template/header.php";
+include "template/sidebar.php"
 ?>
+<!--begin::App Main-->
+<main class="app-main">
+    <!--begin::App Content Header-->
+    <div class="app-content-header">
+        <!--begin::Container-->
+        <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3 class="mb-0">Dashboard</h3>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-end">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            <!--end::Row-->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::App Content Header-->
+    <!--begin::App Content-->
+    <div class="app-content">
+        <!--begin::Container-->
+        <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h3 class="card-title">Data Mahasiswa</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                        <th>No</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Tanggal Lahir</th>
+                        <th>No Telp</th>
+                        <th>Email</th>
+                        <th>Prodi</th>
+                        <th>Aksi</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            foreach ($data as $d) : ?>
+                                <tr>
+                                    <td><?= $i++; ?></td>
+                                    <td><?= $d["nim"] ?></td>
+                                    <td><?= $d["nama"] ?></td>
+                                    <td><?= $d["tgl_lahir"] ?></td>
+                                    <td><?= $d["no_telp"] ?></td>
+                                    <td><?= $d["email"] ?></td>
+                                    <td><?= $d["namaProdi"] ?></td>
+                                    <td>
+                                    <div style="display: flex; gap: 5px;">
+                                    <a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>" onclick="return confirm('Yakin ingin hapus?')" class="btn btn-danger">Delete</a>
+                                    <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>" class="btn btn-warning">Edit</a>
+                                    </div>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPADU</title>
-</head>
-<body>
-    <h1>DATA MAHASISWA</h1>
-    <br>
-    <a href = "tambahmahasiswa.php">tambah</a>
-    <table border="1" cellspacing="0" cellpadding = "1">
-        <thead>
-        <tr>
-            <th>No</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Tanggal Lahir</th>
-            <th>No Telp</th>
-            <th>Email</th>
-            <th>Prodi</th>
-            <th>Aksi</th>
-    </thead>
-    <tbody>
-
-    <?php
-    $i = 1;
-    foreach ($data as $d) : ?>
-
-    <tr>
-    <td><?= $i++; ?></td>
-    <td><?= $d["nim"] ?></td>
-    <td><?= $dgit["nama"] ?></td>
-    <td><?= $d["tgl_lahir"] ?></td>
-    <td><?= $d["no_telp"] ?></td>
-    <td><?= $d["email"] ?></td>
-    <td><?= $d["namaProdi"] ?></td>
-    <td><a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>"
-    onclick="return confirm('Yakin ingin hapus?')">Delete</a> |
-    <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a></td>
-    </tr>
-    <?php endforeach; ?>
-        </tbody>
-    </table>
-    <a href = "logout.php">Logout</a>
-</body>
-</html>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+                <!-- /.col -->
+            </div>
+            <!-- /.row (main row) -->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::App Content-->
+</main>
+<!--end::App Main-->
+<?php
+include "template/footer.php";
+?>
---
+++ prodycopy.php
@@ -0,0 +1,39 @@
+<?php
+include "koneksi.php";
+
+$query = "SELECT * FROM prodi";
+$data = ambildata($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPADU POLIBAN</title>
+</head>
+<body>
+    <h1>DATA PRODI</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding"S">
+        <thead>
+            <th>id</th>
+            <th>nama</th>
+            <th>kaprodi</th>
+            <th>jurusan</th>
+    </thead>
+    <tbody>
+        <?php
+        $i = 1;
+        foreach ($data as $d) : ?>
+        <tr>
+            <td><?php echo $i++; ?></td>
+            <td><?php echo $d["nama"] ?></td>
+            <td><?php echo $d["kaprodi"] ?></td>
+            <td><?php echo $d["jurusan"] ?></td>
+        </tr>
+        <?php endforeach ; ?>
+        </tbody>
+    </table>
+</body>
+</html>
